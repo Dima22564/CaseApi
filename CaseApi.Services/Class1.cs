e2cf1c203d262ff8baaa9c8eb0ec659b8a241bd5
Dmitriy <drakon-sayron@mail.ru>
2020-12-23 01:00:29 +0300

    Initial commit

---
+++ CaseApi.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CaseApi.Services
+{
+    public class Class1
+    {
+    }
+}
---
+++ CaseApi.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace CaseApi.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
